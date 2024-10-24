828f17883051195265274f6297b7b6b4e443261b
unknown <faridwilhelmzimmermann1111@gmail.com>
2024-10-24 14:00:51 +0200

    added state to menu component to hold chosen difficulty and gametype

--- client/src/components/menu/Menu.tsx
+++ client/src/components/menu/Menu.tsx
@@ -4,27 +4,30 @@ import {useState} from "react";
 
 //Type Declarations
 type MenuProps = {
-    gameState: boolean,
-    endGame: () => void,
-    clearCanvas:boolean
 }
 
 
 export default function Menu(props) {
 
+    const [difficulty, setDifficulty] = useState(0);
+    const [gameType, setGameType] = useState(0);
+
     return (<>
         <div>
             <label>
                Computer 
-            <input type="checkbox" />
+            <input type="checkbox" value={"0"} onChange={ e => setGameType(parseInt(e.target.value))} checked={gameType === 0}/>
             </label>
             <label>
                Multiplayer 
-            <input type="checkbox" />
-            </label>
+               <input type="checkbox" value={"1"} onChange={ e => setGameType(parseInt(e.target.value))} checked={gameType === 1}/>
+               </label>
         </div>
         <div>
-            <input type="range" min="1" max="100" value="0"/>
+            <label>
+                Difficulty
+            <input type="range" min="1" max="100" value={difficulty} onChange={e => setDifficulty(parseInt(e.target.value))} disabled={gameType === 1}/>
+            </label>
         </div>
     </>)
 }
