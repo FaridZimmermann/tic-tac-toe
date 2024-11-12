1e729ba4068a70a8779a2f514404ee998b8849e7
unknown <faridwilhelmzimmermann1111@gmail.com>
2024-11-12 15:27:47 +0100

    Start of implementation of negamax algorithm with alpha/beta pruning

--- client/src/helpers/negamax.js
+++ client/src/helpers/negamax.js
@@ -1,1 +1,18 @@
 console.log("abc")
+import checkGameState from "./checkGameState"
+
+function evaluate(board) {
+    if (checkGameState(board, 1)) return 10;
+    if (checkGameState(board, 2)) return -10;
+    return 0;
+}
+
+function boardIsFull(board) {
+    return board.filter(row => row.filter(cell => cell !== 0).length === 3).length === 
+}
+function negamax(board, depth, alpha, beta, color) {
+    
+    const score = evaluate(board);
+
+    if(score === 10 || score === -10 || )
+}
